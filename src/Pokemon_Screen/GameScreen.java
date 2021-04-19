7213c6490909a1ed5782140e19f2e04dfadb19b7
TonyShi12345 <77766339+TonyShi12345@users.noreply.github.com>
2021-04-18 20:25:46 -0700

    third change

--- src/Pokemon_Screen/GameScreen.java
+++ src/Pokemon_Screen/GameScreen.java
@@ -10,7 +10,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GameScreen extends AbstractScreen+1{
+public class GameScreen extends AbstractScreen{
     private Actor actor;
     private TileMap map;
     private Texture standsouth;
